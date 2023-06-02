5b38a16391837a7b380c149fe106f110528bacc6
nkawaller <kawaller@gmail.com>
2023-06-02 19:21:35 -0400

    finish chunks of bytecode

--- value.c
+++ value.c
@@ -9,4 +9,23 @@ void initValueArray(ValueArray* array) {
   array->count = 0;
 }
 
-/* TODO: writeValueArray */
+void writeValueArray(ValueArray* array, Value value) {
+  if (array->capacity < array->count + 1) {
+    int oldCapacity = array->capacity;
+    array->capacity = GROW_CAPACITY(oldCapacity);
+    array->values = GROW_ARRAY(Value, array->values,
+                               oldCapacity, array->capacity);
+  }
+
+  array->values[array->count] = value;
+  array->count++;
+}
+
+void freeValueArray(ValueArray* array) {
+  FREE_ARRAY(Value, array->values, array->capacity);
+  initValueArray(array);
+}
+
+void printValue(Value value) {
+  printf("%g", value);
+}
