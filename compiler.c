597dde2c9a6296063852a402d1aeb1f024fe30d6
nkawaller <kawaller@gmail.com>
2023-06-13 18:43:15 -0400

    start parsing prefix expressions

--- compiler.c
+++ compiler.c
@@ -14,6 +14,12 @@ typedef struct {
 
 Parser parser;
 
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+  return compilingChunk;
+}
+
 static void errorAt(Token* token, const char* message) {
   if (parser.panicMode) return;
   parser.panicMode = true;
@@ -59,12 +65,40 @@ static void consume(TokenType type, const char* message) {
   errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte) {
+  writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+  emitByte(byte1);
+  emitByte(byte2);
+}
+
+static void emitReturn() {
+  emitByte(OP_RETURN);
+}
+
+static void endCompiler() {
+  emitReturn();
+}
+
+static void number() {
+  double value = strtod(parser.previous.start, NULL);
+  emitConstant(value);
+}
+
+static void expression() {
+  // TODO
+}
+
 bool compile(const char* source, Chunk* chunk) {
   initScanner(source);
+  compilingChunk = chunk;
   parser.hadError = false;
   parser.panicMode = false;
   advance();
   expression();
   consume(TOKEN_EOF, "Expect end of expression.");
+  endCompiler();
   return !parser.hadError;
 }
